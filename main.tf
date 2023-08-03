c68a2f28083c5120ce9b67ed6a6af9f3afb56cad
arun-ramesh2 <arun.ramesh2@hotmail.com>
2023-08-03 16:53:22 +0530

    Update main.tf
    Module security group

--- main.tf
+++ main.tf
@@ -16,11 +16,14 @@ data "aws_ami" "app_ami" {
 data "aws_vpc" "default" {
   default = true
 }
+
+
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t3.nano"
 
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
     Name = "HelloWorld"
@@ -31,6 +34,19 @@ resource "aws_security_group" "blog" {
   description = "AWS security tab"
   
   vpc_id = data.aws_vpc.default.id
+}
+
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.0"
+  name    = "blog_new"
+
+ vpc_id = data.aws_vpc.default.id
+ ingress_rules       = ["http-80-tcp","https-443-tcp"]
+ ingress_cidr_blocks = ["0.0.0.0/0"] 
+
+ egress_rules        = ["all-all"]
+ egress_cidr_blocks  = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
